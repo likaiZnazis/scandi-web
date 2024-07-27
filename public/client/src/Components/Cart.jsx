1d9cf322518996d808a811da63bb923eb4a378e7
likaiZnazis <Maris.Vitols_2@edu.rtu.lv>
2024-07-27 07:59:26 +0300

    Cart design is done.
    Need to add couple of functionallity

--- public/client/src/Components/Cart.jsx
+++ public/client/src/Components/Cart.jsx
@@ -4,13 +4,12 @@ import '../assets/css/cart.css';
 class Cart extends Component {
 
   handleQuantityChange = (productId, change) => {
-    // Call a function passed via props to handle quantity changes
     this.props.updateQuantity(productId, change);
   };
 
   calculateTotalPrice = () => {
     return this.props.cartItems.reduce((total, item) => {
-      const itemPrice = item.prod_prices.find(price => price.currency.symbol === this.getCurrencySymbol()); // Adjust as per desired currency symbol
+      const itemPrice = item.prod_prices.find(price => price.currency.symbol === this.getCurrencySymbol());
       if (itemPrice) {
         return total + itemPrice.amount * item.quantity;
       }
@@ -19,7 +18,6 @@ class Cart extends Component {
   };
 
   getCurrencySymbol = () => {
-    // Assuming all products have prices in the same currency, we use the first product's currency symbol.
     const { cartItems } = this.props;
     if (cartItems.length > 0) {
       const itemPrice = cartItems[0].prod_prices.find(price => price.currency.symbol);
@@ -27,7 +25,7 @@ class Cart extends Component {
         return itemPrice.currency.symbol;
       }
     }
-    return "$"; // Default to "$" if no currency symbol is found
+    return "$";
   };
 
   setHexColor = (color) => color.includes("#") ? color : `#${color}`;
@@ -58,61 +56,65 @@ class Cart extends Component {
   };
 
   render() {
-    const { cartItems, visibility } = this.props;
+    const { cartItems, visibility, toggleCartVisibility } = this.props;
 
     return (
-      <div className="cart-modal" style={{ display: visibility ? "block" : "none" }}>
-        <div className="cart">
-          <div className="cart-header">
-            <span className="cart-title">My Bag</span>
-            <span className="cart-comma">, </span>
-            <span className="cart-item-count">
-              {cartItems.length === 0
-                ? 'empty'
-                : `${cartItems.length} ${cartItems.length > 1 ? 'items' : 'item'}`}
-            </span>
-          </div>
-          <div className="cart-product">
-            {cartItems.map((product, index) => (
-              <div key={index} className="cart-product-row">
-                <div className="cart-product-detail">
-                  <h4 className="cart-product-title">{product.name}</h4>
-                  <div className="cart-product-price">
-                    {product.prod_prices.map((price, idx) => (
-                      <p className="product-detail-price" key={idx}>
-                        {price.currency.symbol}{price.amount}
-                      </p>
-                    ))}
-                  </div>
+      <>
+        {visibility && <div className="overlay" onClick={toggleCartVisibility}></div>}
+        <div className="cart-modal" style={{ display: visibility ? "block" : "none" }}>
+          <div className="cart">
+            <div className="cart-header">
+              <span className="cart-title">My Bag</span>
+              <span className="cart-comma">, </span>
+              <span className="cart-item-count">
+                {cartItems.length === 0
+                  ? ''
+                  : `${cartItems.length} ${cartItems.length > 1 ? 'items' : 'item'}`}
+              </span>
+            </div>
+            <div className="cart-product">
+              {cartItems.map((product, index) => (
+                <div key={index} className="cart-product-row">
+                  <div className="cart-product-detail">
+                    <h4 className="cart-product-title">{product.name}</h4>
+                    <div className="cart-product-price">
+                      {product.prod_prices.map((price, idx) => (
+                        <p className="cart-product-detail-price" key={idx}>
+                          {price.currency.symbol}{price.amount}
+                        </p>
+                      ))}
+                    </div>
                     {this.renderAttributes(product.attributes, product.selectedAttributes)}
-                </div>
-                    <div className="cart-product-quantity">
+                  </div>
+                  <div className="cart-product-quantity">
                     <button
-                        className="cart-product-button increase"
-                        onClick={() => this.handleQuantityChange(product.product_id, 1)}
+                      className="cart-product-button increase"
+                      onClick={() => this.handleQuantityChange(product.product_id, 1)}
                     >
-                        +
+                      +
                     </button>
                     <span>{product.quantity}</span>
                     <button
-                        className="cart-product-button decrease"
-                        onClick={() => this.handleQuantityChange(product.product_id, -1)}
+                      className="cart-product-button decrease"
+                      onClick={() => this.handleQuantityChange(product.product_id, -1)}
                     >
-                        -
+                      -
                     </button>
-                    </div>
-                    <div className="cart-product-image">
+                  </div>
+                  <div className="cart-product-image">
                     <img src={product.gallery[0]} alt={product.name} />
-                    </div>
+                  </div>
                 </div>
-            ))}
+              ))}
+            </div>
+            <div className="cart-price">
+              <p className="cart-total">Total</p>
+              <p className="cart-total-price">{this.getCurrencySymbol()}{this.calculateTotalPrice()}</p>
+            </div>
+            <button className="cart-place-order">PLACE ORDER</button>
           </div>
-          <div className="cart-price">
-            <p className="cart-total">Total</p><p className="cart-total-price">{this.getCurrencySymbol()}{this.calculateTotalPrice()}</p>
-          </div>
-          <button className="cart-place-order">PLACE ORDER</button>
         </div>
-      </div>
+      </>
     );
   }
 }
